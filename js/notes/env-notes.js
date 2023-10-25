277c6f70c5231f8798ae29a687b85168576dfd48
Johnny <mrtnz.jn@gmail.com>
2023-10-24 21:18:39 -0500

    completed notes on .env file and created .env and .env.example files. Also, installed MongoDB to local machine

---
+++ js/notes/env-notes.js
@@ -0,0 +1,10 @@
+// TO ACCESS ENV FILE:
+
+// require("dotenv").config();
+
+//^^^ WILL MAKE DATA AVAILABLE BY USING:
+
+    // process.env.nameOfVariableInEnvFile
+
+
+// env.example IS LIKE application.properties.example IN JAVA.    
