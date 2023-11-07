bb6e846dc9aee2a687bdd5d886ffb7b8f7731549
Johnny <mrtnz.jn@gmail.com>
2023-11-07 10:41:08 -0600

    refactored authController to create a jwt token and save it to the cookie after successful login.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,6 +3,9 @@ const loging = require("../Models/login");
 const bcrypt = require("bcrypt");
 const userController = require("../Controllers/userController.js");
 const userRouter = require("../Routes/userRoutes");
+const jwt = require("jsonwebtoken");
+
+const jwtSecret = process.env.JWT_SECRET;
 
 
 
@@ -15,22 +18,49 @@ const redernLoginForm = (req, res) => {
 
 
 // **************** LOGIN REQUEST ************************
+// const loginAuth = async (req, res) => {
+//     try {
+//         const checkIfUserExists = await User.findOne({ email: req.body.email });
+//         if(!checkIfUserExists){
+//             console.log("Invalid credientials.");
+//             res.redirect("/login");
+//         } else {
+
+//             // COMPARE HASHED PASSWORD
+//             const passwordCompare = await bcrypt.compare(req.body.password, checkIfUserExists.password)
+//             if(passwordCompare){
+//                 console.log(`Login Successful. ${checkIfUserExists._id}`);
+//                 res.redirect(`users/${checkIfUserExists._id}`);
+//             } else {
+//                 console.log("Wrong email or Password.");
+//                 res.redirect("/login");
+//             }
+//         }
+        
+//     } catch (error) {
+//         console.log(error);
+//     }
+// } 
+
 const loginAuth = async (req, res) => {
     try {
-        const checkIfUserExists = await User.findOne({ email: req.body.email });
-        if(!checkIfUserExists){
-            console.log("User does not exist.");
+        const userInDb = await User.findOne({ email: req.body.email });
+        if(!userInDb){
+            console.log("Invalid credientials.");
             res.redirect("/login");
         } else {
 
             // COMPARE HASHED PASSWORD
-            const passwordCompare = await bcrypt.compare(req.body.password, checkIfUserExists.password)
-            if(passwordCompare){
-                console.log("Login Successful.");
-                res.redirect("/blogs");
-            } else {
+            const passwordCompare = await bcrypt.compare(req.body.password, userInDb.password)
+            if(!passwordCompare){
                 console.log("Wrong email or Password.");
                 res.redirect("/login");
+            } else {
+                // SAVING A TOKEN TO THE COOKIE
+                const token = jwt.sign({userId: userInDb._id}, jwtSecret);
+                res.cookie("token", token, {httpOnly: true});
+                console.log("Login Successful.");
+                res.redirect(`users/${userInDb._id}`);
             }
         }
         
